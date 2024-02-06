009ff7d6b0a2f6fca06724d6366f3227f89e9ad8
Henry Westhoff-Lewis <henry@192.168.1.101>
2024-02-06 09:30:35 +0000

    Task 7 article votes patch

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
 
-const SingleArticle = ({article}) => {
+const SingleArticle = ({article, articleVoteHandler}) => {
     return (
         <div className="singleArticleDiv">
 
             <p>{article.title}</p>
             <Link to={`/articles/${article.article_id}`}>
             <img src={article.article_img_url} alt="" />
+            </Link>
             <p>Author: {article.author}</p>
             <p>Topic: {article.topic}</p>
             <p>votes: {article.votes}</p>
+            <button onClick={() => {articleVoteHandler(article.article_id, 'inc')}}>Vote up</button>
+            <button onClick={() => {articleVoteHandler(article.article_id, 'dec')}}>Vote down</button>
             
-            </Link>
+            
         </div>
     )
 }
