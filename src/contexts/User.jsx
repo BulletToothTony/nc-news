3028d03988d26b5e54c46c99388d49e0f1a3d670
Henry Westhoff-Lewis <henry@192.168.1.101>
2024-02-08 16:34:38 +0000

    User login screen

--- src/contexts/User.jsx
+++ src/contexts/User.jsx
@@ -1,9 +1,10 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState('happyamy2016');
+
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
